42f31c92e027557fee7e90001399c7c1abdfeba8
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2023-04-09 23:23:23 +0530

    feat(main.rs): add Quote struct and parse response into it fix(main.rs): remove unused import statement
    chore(termoil): add output-bin-termoil to .gitignore
    fix(main.rs): remove unused anime field in Quote struct
    
    chore(termoil): update output-test-bin-termoil fingerprint with new warning message
    
    chore: update incremental build artifacts
    
    build: add incremental build artifacts to target/debug directory

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 use clap::Parser;
+use serde::Deserialize;
 
+// use serde::Deserialize;
 
-
-
+#[derive(Deserialize)]
+struct Quote {
+    quote: String,
+    character: String,
+    anime: String,
+}
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -16,6 +22,17 @@ struct Args {
 async fn main() {
     let args = Args::parse();
 
-  
+    let request_url = "https://animechan.vercel.app/api/random";
+
+    let response = reqwest::get(request_url)
+        .await
+        .expect("Failed to get response");
+
+
+    //parse the  response into a Quote struct
+    let quote: Quote = response.json().await.expect("Failed to parse response");
+
+    println!("{} - {}", quote.quote, quote.character);
+
     println!("Hello, {}!", args.query);
 }
