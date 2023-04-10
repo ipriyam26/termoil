2d85999aecc825221bb62dc2a62856603e1fdfea
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2023-04-10 12:34:23 +0530

    that's how we read basic command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+//NOTE - So for making a command line tool the first thing I need is, for a way to take user input, upon a little reasearch I found that I can use the std library to do that.
+
 fn main(){
-    println!("Hello, world!");
+    let arguments = std::env::args().collect::<Vec<String>>();
+    println!("{:?}", arguments);
 }
