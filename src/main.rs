65212fc17fa670e6111508911b38530f09c0aeed
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2023-04-11 16:19:19 +0530

    ran successfully response looking great

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 
 use clap::Parser;
 use serde::{Deserialize, Serialize};
-
+use serde_json::to_string_pretty;
 use std::error::Error;
 
 #[derive(Deserialize, Debug,Serialize)]
@@ -53,14 +53,14 @@ async fn main() -> Result<(), Box<dyn Error>> {
     );
     let b = reqwest::get(url).await?;
     let results: Vec<ApiResponse> = b.json().await.expect("Error while parsing json");
-
-    println!("{:?}", results);
+    let pretty_results = to_string_pretty(&results[0].meanings).expect("Error while pretty-printing results");
+    println!("{}", pretty_results);
     Ok(())
     // println!("Query: {:?}", arguments.query);
 }
 
-// Ran successfully with
-// ❯ cargo run -- -q "matter"
+// Ran successfully
+// ❯ cargo run -- -q "rust"
 //    Compiling termoil v0.1.0 (/Users/ipriyam26/Programing/Rust/termoil)
 // warning: structure field `partOfSpeech` should have a snake case name
 //   --> src/main.rs:26:5
@@ -71,6 +71,42 @@ async fn main() -> Result<(), Box<dyn Error>> {
 //    = note: `#[warn(non_snake_case)]` on by default
 
 // warning: `termoil` (bin "termoil") generated 1 warning
-//     Finished dev [unoptimized + debuginfo] target(s) in 2.44s
-//      Running `target/debug/termoil -q matter`
-// [ApiResponse { word: "matter", phonetic: Some("/ˈmætə/"), phonetics: Some([Phonetic { text: "/ˈmætə/", audio: Some("") }, Phonetic { text: "/ˈmætɚ/", audio: Some("https://api.dictionaryapi.dev/media/pronunciations/en/matter-us.mp3") }]), origin: None, meanings: [Meaning { partOfSpeech: "noun", definitions: [Definition { definition: "Substance, material.", example: None, synonyms: Some([]), antonyms: Some([]) }, Definition { definition: "A condition, subject or affair, especially one of concern.", example: Some("What's the matter?; \u{a0} state matters"), synonyms: Some([]), antonyms: Some([]) }, Definition { definition: "An approximate amount or extent.", example: Some("I stayed for a matter of months."), synonyms: Some([]), antonyms: Some([]) }, Definition { definition: "The essence; the pith; the embodiment.", example: None, synonyms: Some([]), antonyms: Some([]) }, Definition { definition: "Inducing cause or reason, especially of anything disagreeable or distressing.", example: None, synonyms: Some([]), antonyms: Some([]) }, Definition { definition: "Pus.", example: None, synonyms: Some([]), antonyms: Some([]) }] }, Meaning { partOfSpeech: "verb", definitions: [Definition { definition: "To be important.", example: Some("Sorry for pouring ketchup on your clean white shirt! - Oh, don't worry, it does not matter."), synonyms: Some([]), antonyms: Some([]) }, Definition { definition: "(in negative constructions) To care about, to mind; to find important.", example: None, synonyms: Some([]), antonyms: Some([]) }, Definition { definition: "To form pus or matter, as an abscess; to maturate.", example: None, synonyms: Some([]), antonyms: Some([]) }] }] }]
+//     Finished dev [unoptimized + debuginfo] target(s) in 1.45s
+//      Running `target/debug/termoil -q rust`
+// [
+//   {
+//     "partOfSpeech": "noun",
+//     "definitions": [
+//       {
+//         "definition": "The deteriorated state of iron or steel as a result of moisture and oxidation.",
+//         "example": "The rust on my bicycle chain made cycling to work very dangerous.",
+//         "synonyms": [],
+//         "antonyms": []
+//       },
+//       {
+//         "definition": "A similar substance based on another metal (usually with qualification, such as \"copper rust\").",
+//         "example": "aerugo. Green or blue-green copper rust; verdigris. (American Heritage Dictionary, 1973)",
+//         "synonyms": [],
+//         "antonyms": []
+//       },
+//       {
+//         "definition": "A reddish-brown color.",
+//         "example": null,
+//         "synonyms": [],
+//         "antonyms": []
+//       },
+//       {
+//         "definition": "A disease of plants caused by a reddish-brown fungus.",
+//         "example": null,
+//         "synonyms": [],
+//         "antonyms": []
+//       },
+//       {
+//         "definition": "Damage caused to stamps and album pages by a fungal infection.",
+//         "example": null,
+//         "synonyms": [],
+//         "antonyms": []
+//       }
+//     ]
+//   }
+// ]
