131e0e3376e097bc4a9c48ea4a8c6630f71440b6
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2023-04-11 16:13:06 +0530

    still error even after adding async

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ struct Args {
 }
 
 
- fn main(){
+async fn main() {
     let arguments = Args::parse();
     let url = format!(
         "https://api.dictionaryapi.dev/api/v2/entries/en/{}",
@@ -26,45 +26,30 @@ struct Args {
     let results= b.json().await.expect("Error while parsing json");
 
     println!("{:?}", results);
-
+  
     // println!("Query: {:?}", arguments.query);
 }
 
-//ERROR -
-// warning: unused import: `std::error::Error`
-//  --> src/main.rs:7:5
-//   |
-// 7 | use std::error::Error;
-//   |     ^^^^^^^^^^^^^^^^^
-//   |
-//   = note: `#[warn(unused_imports)]` on by default
-
-// error[E0728]: `await` is only allowed inside `async` functions and blocks
-//   --> src/main.rs:25:30
-//    |
-// 19 |  fn main(){
-//    |     ---- this is not `async`
-// ...
-// 25 |     let b = reqwest::get(url).await?;
-//    |                              ^^^^^^ only allowed inside `async` functions and blocks
-
-// error[E0728]: `await` is only allowed inside `async` functions and blocks
-//   --> src/main.rs:26:26
-//    |
-// 19 |  fn main(){
-//    |     ---- this is not `async`
-// ...
-// 26 |     let results= b.json().await.expect("Error while parsing json");
-//    |                          ^^^^^^ only allowed inside `async` functions and blocks
-
-// error[E0277]: the `?` operator can only be used in a function that returns `Result` or `Option` (or another type that implements `FromResidual`)
+// Error - error[E0277]: the `?` operator can only be used in an async function that returns `Result` or `Option` (or another type that implements `FromResidual`)
 //   --> src/main.rs:25:36
-//    |
-// 19 |  fn main(){
-//    |  --------- this function should return `Result` or `Option` to accept `?`
-// ...
-// 25 |     let b = reqwest::get(url).await?;
-//    |                                    ^ cannot use the `?` operator in a function that returns `()`
-//    |
-//    = help: the trait `FromResidual<Result<Infallible, reqwest::Error>>` is not implemented for `()`
+//   |
+// 19 |   async fn main() {
+//   |  _________________-
+// 20 | |     let arguments = Args::parse();
+// 21 | |     let url = format!(
+// 22 | |         "https://api.dictionaryapi.dev/api/v2/entries/en/{}",
+// ...  |
+// 25 | |     let b = reqwest::get(url).await?;
+//   | |                                    ^ cannot use the `?` operator in an async function that returns `()`
+// ...  |
+// 30 | |     // println!("Query: {:?}", arguments.query);
+// 31 | | }
+//   | |_- this function should return `Result` or `Option` to accept `?`
+//   |
+//   = help: the trait `FromResidual<Result<Infallible, reqwest::Error>>` is not implemented for `()`
 
+// error[E0752]: `main` function is not allowed to be `async`
+//  --> src/main.rs:19:1
+//   |
+// 19 | async fn main() {
+//   | ^^^^^^^^^^^^^^^ `main` function is not allowed to be `async`
