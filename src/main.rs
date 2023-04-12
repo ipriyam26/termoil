c1230e609da7e9be404da009a919a5c59ffc088c
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2023-04-12 22:27:42 +0530

    I am getting response now

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
 //NOTE - We should now try with the actual service we would like to use it in, so lets check the open ai api.
 
 use clap::Parser;
-use serde_json::to_string_pretty;
-use std::{error::Error, env};
 use dotenv::dotenv;
+use reqwest::{
+    header::{self, HeaderMap, HeaderValue},
+    Client,
+};
+use serde::Deserialize;
+use serde_json::{json};
+use std::{env, error::Error};
+
+#[derive(Debug, Deserialize)]
+struct ApiResponse {
+    choices: Vec<Choice>,
+}
+
+
+
+#[derive(Debug, Deserialize)]
+struct Choice {
+    message: Message,
+}
+
+#[derive(Debug, Deserialize)]
+struct Message {
+    content: String,
+}
 
 
 #[derive(Parser, Debug)]
@@ -19,9 +41,42 @@ async fn main() -> Result<(), Box<dyn Error>> {
     dotenv().ok();
     let arguments = Args::parse();
     let open_ai_api_key = env::var("OPEN_AI_API_KEY").expect("OPEN_AI_API_KEY not set");
+    let query = arguments.query.to_owned();
+    let client = Client::new();
 
-   
+    let url = "https://api.openai.com/v1/chat/completions";
+
+    let headers: HeaderMap<HeaderValue> = header::HeaderMap::from_iter(vec![
+        (header::CONTENT_TYPE, "application/json".parse().unwrap()),
+        (
+            header::AUTHORIZATION,
+            format!("Bearer {}", open_ai_api_key).parse().unwrap(),
+        ),
+    ]);
+
+    let body = json!(
+        {
+            "model":"gpt-3.5-turbo",
+            "messages":[{
+                "role":"user",
+                "content": query,
+            }]
+        }
+    );
+    // println!("{:#?}", &body);
+
+    let response: ApiResponse = client
+        .post(url)
+        .headers(headers)
+        .json(&body)
+        .send()
+        .await?
+        .json()
+        .await?;
+
+
+    println!("{}", &response.choices[0].message.content);
+
     Ok(())
     // println!("Query: {:?}", arguments.query);
 }
-
