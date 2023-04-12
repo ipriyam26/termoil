18d6cc4618121732c60f86c9f2680d3495546701
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2023-04-12 22:51:18 +0530

    added a nice system command

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-//NOTE - We should now try with the actual service we would like to use it in, so lets check the open ai api.
+//NOTE - lets allow the user to provide max_tokens
 
 use clap::Parser;
 use dotenv::dotenv;
@@ -7,15 +7,13 @@ use reqwest::{
     Client,
 };
 use serde::Deserialize;
-use serde_json::{json};
+use serde_json::json;
 use std::{env, error::Error};
 
 #[derive(Debug, Deserialize)]
 struct ApiResponse {
     choices: Vec<Choice>,
 }
-
-
 
 #[derive(Debug, Deserialize)]
 struct Choice {
@@ -27,13 +25,15 @@ struct Message {
     content: String,
 }
 
-
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     // query the user wants to enter
     #[arg(short, long)]
     query: String,
+
+    #[arg(short, long)]
+    tokens: Option<u32>,
 }
 
 #[tokio::main]
@@ -42,6 +42,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let arguments = Args::parse();
     let open_ai_api_key = env::var("OPEN_AI_API_KEY").expect("OPEN_AI_API_KEY not set");
     let query = arguments.query.to_owned();
+    let tokens = arguments.tokens.unwrap_or(200).to_owned();
     let client = Client::new();
 
     let url = "https://api.openai.com/v1/chat/completions";
@@ -57,10 +58,15 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let body = json!(
         {
             "model":"gpt-3.5-turbo",
-            "messages":[{
+            "messages":[
+                {"role": "system",
+                "content": "Act as a terminal expert, if user is asking for command give COMMAND ONLY"},
+            {
                 "role":"user",
                 "content": query,
-            }]
+            }
+            ],
+            "max_tokens": tokens,
         }
     );
     // println!("{:#?}", &body);
@@ -73,7 +79,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .await?
         .json()
         .await?;
-
 
     println!("{}", &response.choices[0].message.content);
 
